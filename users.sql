7390cd2d54f6808710193c9a28855d437b59af19
zzleomar <dmy.leomar@gmail.com>
2019-01-24 15:45:10 -0400

    backup

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 15-01-2019 a las 13:32:54
+-- Tiempo de generación: 24-01-2019 a las 15:43:40
 -- Versión del servidor: 5.7.24-0ubuntu0.18.04.1
 -- Versión de PHP: 7.2.10-0ubuntu0.18.04.1
 
@@ -30,6 +30,10 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` text NOT NULL,
   `birthday` date NOT NULL,
+  `gender` text NOT NULL,
+  `note` text NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `date` date NOT NULL,
   `createdAt` date NOT NULL,
   `updatedAt` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -38,8 +42,12 @@ CREATE TABLE `users` (
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `birthday`, `createdAt`, `updatedAt`) VALUES
-(4, 'aqui esta user2', '2019-01-15', '2019-01-15', '2019-01-15');
+INSERT INTO `users` (`id`, `username`, `birthday`, `gender`, `note`, `status`, `date`, `createdAt`, `updatedAt`) VALUES
+(7, 'gerson', '2018-12-30', 'Male', 'asdasd', 0, '2019-01-23', '2019-01-23', '2019-01-23'),
+(9, 'Luis', '2018-12-30', 'Femele', 'ascxasd', 1, '2019-01-23', '2019-01-23', '2019-01-24'),
+(12, 'Sore', '2018-12-29', 'Femele', 'asdasd', 1, '2019-01-23', '2019-01-23', '2019-01-24'),
+(13, 'wladi', '2018-12-30', 'Male', 'asdasd', 1, '2019-01-24', '2019-01-24', '2019-01-24'),
+(14, 'Dani', '2019-01-30', 'Femele', 'sdfdsfdsf', 1, '2019-01-24', '2019-01-24', '2019-01-24');
 
 --
 -- Índices para tablas volcadas
@@ -59,7 +67,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
